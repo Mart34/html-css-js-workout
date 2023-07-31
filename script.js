13d4be693b55f683045b7b70c38358b8f6e24628
Marta Tran <tran.marta@gmail.com>
2023-07-31 16:53:25 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello from Nerdbord!')
-
-function changeColor()
-{
-    setInterval(console.log, 3000);
-    document.getElementById(".box").style.color = "blue", "green", "yellow", "orange";
-}
